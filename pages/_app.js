0efb7e470e94e8535f45efe89af86f36b3a55a61
Kevin Schans <kevinvdsd@hotmail.com>
2023-10-31 17:24:49 +0100

    Try fix deploy

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import { useRouter } from 'next/router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/dist/jquery.min.js';
-import "../src/scss/style.scss";
+import "src/scss/style.scss";
 
 import Home from './home';
 
+
 function Layout ({ children }) {
+
   const router = useRouter();
 
   if (router.pathname === '/') {
