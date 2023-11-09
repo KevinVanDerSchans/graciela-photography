6db988417d0afd89834e0d2b63ce391ad72a5993
Kevin Schans <kevinvdsd@hotmail.com>
2023-11-09 01:15:08 +0100

    Add animals collection

---
+++ src/data/animalsImages.ts
@@ -0,0 +1,59 @@
+const animalsImages = [
+  {
+    src: '/images/collection/animals/1.jpg',
+    width: 1200,
+    height: 1200,
+  },
+  {
+    src: '/images/collection/animals/2.jpg',
+    width: 1600,
+    height: 1620,
+  },
+  {
+    src: '/images/collection/animals/3.jpg',
+    width: 800,
+    height: 720,
+  },
+  {
+    src: '/images/collection/animals/4.jpg',
+    width: 1000,
+    height: 1020,
+  },
+  {
+    src: '/images/collection/animals/5.jpg',
+    width: 1100,
+    height: 1120,
+  },
+  {
+    src: '/images/collection/animals/6.jpg',
+    width: 1700,
+    height: 1620,
+  },
+  {
+    src: '/images/collection/animals/7.jpg',
+    width: 1700,
+    height: 1620,
+  },
+  {
+    src: '/images/collection/animals/8.jpg',
+    width: 1700,
+    height: 1620,
+  },
+  {
+    src: '/images/collection/animals/9.jpg',
+    width: 1700,
+    height: 1620,
+  },
+  {
+    src: '/images/collection/animals/10.jpg',
+    width: 1700,
+    height: 1620,
+  },
+  {
+    src: '/images/collection/animals/11.jpg',
+    width: 1700,
+    height: 1420,
+  },
+];
+
+export default animalsImages;
