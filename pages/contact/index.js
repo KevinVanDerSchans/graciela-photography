75dd5c815a77e8ae6befbcb8f304e9e34e3ab272
Kevin Schans <kevinvdsd@hotmail.com>
2023-11-16 14:06:28 +0100

    Create contact info and add google map

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -7,12 +7,65 @@ function Contact() {
 
   return (
     <>
-       <Header
+      <Header
         headerLogo={Logo}
       />
 
-      <Footer />
-    </>
+      <main className="ms-main">
+        <div className="ms-page-content">
+
+          <div className="contact-area contact">
+            <div className="container">
+              <div className="contact-area-inner">
+                <div className="row">
+                  <div className="col-lg-6">
+
+                    <div className="content">
+                      <h2 className="title">Are you up <br/> for a PHOTO SHOOT?</h2>
+                      <p className="desc">Share your ideas and questions with me, and I will get back to you as soon as possible! I am here to help. Thanks for your interest, and I am excited to learn more about what you have in mind!</p>
+                    </div>
+
+                    <div className="row contact">
+                      <ul className="col-lg-6 phone">
+                        <li className="tag">Phone:</li>
+                        <li>+34 666 39 41 50</li>
+                      </ul>
+                      <ul className="col-lg-6 email">
+                        <li className="tag">Email:</li>
+                        <li>gracieladelafuentee@gmail.com</li>
+                      </ul>
+                    </div>
+                  </div>
+
+              </div>
+            </div>
+          </div>
+
+          <div className="contact-map-area">
+            <div className="container">
+              <div className="row">
+                <div className="col-12">
+
+                  <div className="contact-map-area-fluid">
+                    <iframe
+                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d801.4114258004773!2d-4.645048230414066!3d36.538541075789816!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd72e21fd1d0ad39%3A0x1d0c5a7732c427e6!2sC.%20R%C3%ADo%20Velillos%2C%2019-3%2C%2029651%20Fuengirola%2C%20M%C3%A1laga!5e0!3m2!1ses!2ses!4v1700135042053!5m2!1ses!2ses"
+                      className="contact-map"
+                      allowfullscreen=""
+                      loading="lazy"
+                      referrerpolicy="no-referrer-when-downgrade"
+                    >
+                    </iframe>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+
+    <Footer />
+  </>
   );
 }
 
