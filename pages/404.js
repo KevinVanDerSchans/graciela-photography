d60ce7ae392d2aa3b9d197c0132e68faa7ac267d
Kevin Schans <kevinvdsd@hotmail.com>
2023-11-19 23:10:48 +0100

    Add Error Page

--- pages/404.js
+++ pages/404.js
@@ -9,12 +9,7 @@ function ErrorPage() {
       <section className="ms-page-content ms-404-page">
         <div className="ms-404--content">
           <h2>Whoops, that page is gone.</h2>
-
-          <p>The page you are looking for does not exist. How you got here is a mystery. Go back to the
-            <a
-              href="https://graciela-photography.vercel.app/">homepage.
-            </a>
-          </p>
+          <p>The page you are looking for does not exist. <br /></p>
         </div>
 
         <div className="ms-404--bg">
